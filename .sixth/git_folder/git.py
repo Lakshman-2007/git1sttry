39bdf461fe307aeabad4c0d42439b6e4f45fb28c
Lakshmana Dhityan <25cu0320026@student.hindustanuniv.ac.in>
2026-06-02 11:14:08 +0530

    first commit

---
+++ .sixth/git_folder/git.py
@@ -0,0 +1,8 @@
+from flask import Flask
+app=Flask(__name__)
+@app.route("/company")
+def company_api():
+    return [{"Name":"Open AI",
+            "Location":"USA",
+            "employees":1000}]
+app.run(host="0.0.0.0",port=8000,debug=True)
